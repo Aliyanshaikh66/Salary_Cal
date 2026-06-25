57af4df51fec652fc30db76069a0363cc787e336
Aliyanshaikh66 <aliyanzahid02@gmail.com>
2026-06-25 14:25:45 +0500

    Add Employee Name

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const calculateSalary = (salary) => salary + (salary * 25 / 100);
+let EmployeeName = prompt("Enter Your Name")
 
 let employeeSalary = Number(prompt("Enter Basic Salary:"));
 
 if (!isNaN(employeeSalary) && employeeSalary > 0) {
     let totalSalary = calculateSalary(employeeSalary);
 
-    console.log("Employee Salary:", employeeSalary);
+    console.log("Employee Name : ",EmployeeName,"Employee Salary: ", employeeSalary);
     console.log("Allowance (10%):", employeeSalary * 0.25);
     console.log("Net Salary:", totalSalary);
 } else {
